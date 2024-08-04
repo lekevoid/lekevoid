e058ba478e41bc86b73cd5b0c2edcd1218bb5bed
broadsign-lekevoid <kevin.gagnon@broadsign.com>
2024-08-04 11:38:47 -0400

    Upgrade to Quasar 2 (still needs work)

--- quasar.conf.js
+++ quasar.conf.js
@@ -8,7 +8,7 @@
 /* eslint-env node */
 const ESLintPlugin = require("eslint-webpack-plugin");
 
-module.exports = function(/* ctx */) {
+module.exports = function (/* ctx */) {
 	return {
 		// https://v1.quasar.dev/quasar-cli/supporting-ts
 		supportTS: false,
@@ -114,9 +114,9 @@ module.exports = function(/* ctx */) {
 			workboxPluginMode: "GenerateSW", // 'GenerateSW' or 'InjectManifest'
 			workboxOptions: {}, // only for GenerateSW
 			manifest: {
-				name: `LeKevoid`,
-				short_name: `LeKevoid`,
-				description: `LeKevoid's website`,
+				name: "LeKevoid",
+				short_name: "LeKevoid",
+				description: "LeKevoid's website",
 				display: "standalone",
 				orientation: "portrait",
 				background_color: "#000000",
